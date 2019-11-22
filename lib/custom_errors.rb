7754c9e4777916fd06ceffe6f9f0ad2ac1d96a7f
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-22 21:43:05 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,23 +1,16 @@
 class Person
-  attr_accessor :partner, :name
-
-  def initialize(name)
-    @name = name
-  end
-
+  ...
+ 
   def get_married(person)
     self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
-    end
+    person.partner = self
   end
-
+ 
+  # Add the following two lines:
   class PartnerError < StandardError
   end
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
